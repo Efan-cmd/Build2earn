573321c1ae3892530a0fb34b79635585b3209f04
Efan-cmd <efanlrcrane@icloud.com>
2025-06-03 21:36:08 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Build2Earn</h1>
+      <p>Create and monetize your Fortnite maps!</p>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Welcome to Build2Earn</h1>);
+root.render(<App />);
