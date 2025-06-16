1b3fbff60ed14af6dd03c6add3ac2e5f2bafddca
Efan-cmd <efanlrcrane@icloud.com>
2025-06-16 23:07:09 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Build2Earn</h1>
-      <p>Create and monetize your Fortnite maps!</p>
-    </div>
+    <Router>
+      <nav style={{ padding: "10px", background: "#f8f8f8" }}>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/pricing">Pricing</Link> |{" "}
+        <Link to="/login">Login</Link> |{" "}
+        <Link to="/register">Register</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
