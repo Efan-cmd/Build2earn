020b5657998c2b0c420db51c516ecadacedf8513
Efan-cmd <efanlrcrane@icloud.com>
2025-06-16 22:55:55 +0100

    Create index.js
    import React from "react";
    import ReactDOM from "react-dom/client";
    
    const App = () => {
      return (
        <div>
          <h1>Welcome to Build2Earn</h1>
          <p>Create and monetize Fortnite maps!</p>
        </div>
      );
    };
    
    const root = ReactDOM.createRoot(document.getElementById("root"));
    root.render(<App />);

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Build2Earn</h1>
+      <p>Create and monetize Fortnite maps!</p>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
