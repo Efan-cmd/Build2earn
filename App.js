eb591299000db740d46c9a02f0f114eef2047939
Efan-cmd <efanlrcrane@icloud.com>
2025-06-17 18:34:12 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Pricing from "./pages/Pricing";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
@@ -11,15 +11,15 @@ function App() {
     <Router>
       <nav style={{ padding: "10px", background: "#f8f8f8" }}>
         <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
         <Link to="/pricing">Pricing</Link> |{" "}
+        <Link to="/dashboard">Dashboard</Link> |{" "}
         <Link to="/login">Login</Link> |{" "}
         <Link to="/register">Register</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
